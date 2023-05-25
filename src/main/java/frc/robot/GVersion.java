151100c77660f6387c633cf3747d7808f799f743
Aleksandr Usanov <60308780+phalanx5563@users.noreply.github.com>
2023-05-25 01:16:38 -0400

    Update GVersion.java

--- src/main/java/frc/robot/GVersion.java
+++ src/main/java/frc/robot/GVersion.java
@@ -5,13 +5,13 @@ public final class GVersion {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Senioritis";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "df0a36a21eda49c7a53b61f5400fc9074ab83b90";
-  public static final String GIT_DATE = "2023-05-24 19:08:24 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-05-24 20:32:02 EDT";
-  public static final long BUILD_UNIX_TIME = 1684974722674L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2023-05-25 01:00:28 EDT";
+  public static final long BUILD_UNIX_TIME = 1684990828507L;
+  public static final int DIRTY = 129;
 
   private GVersion() {}
 }
